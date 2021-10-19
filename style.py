086d610021ec0e5da51ddf655f185a0259e2babb
rehnbea1 <albert.rehnberg@aalto.fi>
2021-10-19 14:51:14 +0100

    lade till kommentarer till styles

--- style.py
+++ style.py
@@ -1,18 +1,15 @@
+#fil för antingen styling av guin eller som samlingssida för olika funktioner, vi får se när jag kommit så långt
 
-def style(self):
-    style = Style()
-    # This will be adding style, and
-    # naming that style variable as
-    # W.Tbutton (TButton is used for ttk.Button).
-    style.configure('W.TButton', font =
-                   ('calibri', 10, 'bold', 'underline'),
-                    foreground = 'red')
 
-    # Style will be reflected only on
-    # this button because we are providing
-    # style only on this Button.
-    ''' Button 1'''
-    btn1 = Button(root, text = 'Quit !',
-                    style = 'W.TButton',
-                 command = root.destroy)
-    btn1.grid(row = 0, column = 3, padx = 100)
+import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
