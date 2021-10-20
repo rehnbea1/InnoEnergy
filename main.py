7383e04af20647a03002202bf550b509598e6b9c
rehnbea1 <albert.rehnberg@aalto.fi>
2021-10-20 10:32:59 +0100

    fixade popup fönster, if else för rätt fil soon tm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #fil för att skriva python kod
+
+import myfunctions
 import tkinter as tk
 from tkinter import *
-from tkinter import filedialog as fd
 from tkinter.ttk import *
 
 
@@ -11,13 +12,42 @@ gui.geometry("500x500")
 
 my_label = Label(gui, text = "Welcome to read file")
 
+
+#Läsa, skriva och ändra csv-filen sker inne i denna funktion
 def Delta():
-    filename = Select_file()
+    file_1 = myfunctions.Select_file(gui)
+
+    while file_1 == 0:
+        Label(gui, text="the file you wanted to read didn't work. Try again?")
+        Choice = myfunctions.option_popup(gui)
+        if Choice == False:
+            file = Button(gui, text='Browse file', command = myfunctions.Select_file(gui)).pack()
+        else:
+            print("ok")
+            file_2 = myfunctions.Select_file(gui)
+            file_info = Label(gui,text="The file you chose is: " + file_1).pack()
+            myfunctions.Read_file(filename,gui)
     print("ok")
-    Read_file(filename)
+    file_2 = myfunctions.Select_file(gui)
+    file_info = Label(gui,text = "The file you chose is: " + file_1).pack()
+    myfunctions.Read_file(filename,gui)
+
+    while file_2 == False:
+        Label(gui, text="the file you wanted to read didn't work. Try again?")
+        Answer = tk.Entry(master)
+        if Answer == "J":
+            file = Button(gui, text='Browse file', command = myfunctions.Select_file(gui))
+        else:
+            file_2 = "Empty"
+
 
 #Denna gör att filläsningsfunktionen kallas
 file = Button(gui, text='Browse file', command = Delta)
+
+
+
+
+
 
 
 file.pack()
--- myfunctions.py
+++ myfunctions.py
@@ -1,13 +1,34 @@
 #fil för antingen styling av guin eller som samlingssida för olika funktioner, vi får se när jag kommit så långt
 #just nu bara en test-sida
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import filedialog as fd
+from tkinter import messagebox
 
-def Select_file(self):
+def Select_file(gui):
     print("hej")
     filetypes = (('csv-files', '*.pdf'),('All files', '*.*'))
     filename = fd.askopenfilename(title='Open a file',initialdir='documents/',filetypes=filetypes)
-    file_info=Label(gui,text="The file you chose is: " + filename).pack()
+    if filename != '*.csv' :
+        print("this is not a csv file")
+        return 0
+
     return filename
 
-def Read_file(self, filename):
+def Read_file(filename,gui):
+    #try:
     with open(filename,'r') as file:
         file = file.read()
+
+
+    #except FileNotFoundError:
+    #    Label(gui,text="Error! Could not read the file, make sure you selected the right file").pack()
+    #    return False
+    #except UnicodeDecodeError:
+    #    Label(gui,text="Error! Could not decrypt the file, make sure you selected the right file").pack()
+    #    return False
+
+def option_popup(gui):
+    tk.messagebox.askyesno(title="Option", message="Your file is invalid. Do you want to try again?")
+    return
