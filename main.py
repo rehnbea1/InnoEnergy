abb514942af895646e4609f27652544e50a36a74
rehnbea1 <albert.rehnberg@aalto.fi>
2021-10-21 15:41:44 +0100

    fixade funktion som läser csv filer och skapar en dictionary med all data

--- main.py
+++ main.py
@@ -11,31 +11,33 @@ gui.title("Energy management system 1.0")
 gui.geometry("500x500")
 
 my_label = Label(gui, text = "Welcome to read file")
-
-
+print("–––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***––––––")
+print("–––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***–––––––––––––***––––––")
 #Läsa, skriva och ändra csv-filen sker inne i denna funktion
-def Delta():
+def Delta1():
     file_1 = myfunctions.Select_file(gui)
     file_1 = myfunctions.Check_file(file_1,gui)
-    file_info = Label(gui,text="Your selection for File_1: "+ file_1).pack()
+    file_info = Label(gui, text="Your selection for File_1: "+ file_1).pack()
 
+    myfunctions.Read_file(file_1,gui)
+
+def Delta2():
     file_2 = myfunctions.Select_file(gui)
     file_2 = myfunctions.Check_file(file_2,gui)
     file_info = Label(gui,text="Your selection for File_2: "+ file_2).pack()
 
-    myfunctions.Read_file(file_1,gui)
     myfunctions.Read_file(file_2,gui)
 
-
 #Denna gör att filläsningsfunktionen kallas
-file = Button(gui, text='Browse file', command = Delta)
+file1 = Button(gui, text='Browse file_1', command = Delta1)
+file2 = Button(gui, text='Browse file_2', command = Delta2)
+
+my_label.grid(row=0,  column=1)
+file1.grid(row = 1, column = 0)
+file2.grid(row = 1, column = 1)
 
 
-
-
-
-
-
-file.pack()
 my_label.pack()
+file1.pack()
+file2.pack()
 gui.mainloop()
--- myfunctions.py
+++ myfunctions.py
@@ -1,20 +1,54 @@
 #fil för antingen styling av guin eller som samlingssida för olika funktioner, vi får se när jag kommit så långt
 #just nu bara en test-sida
+import csv
 import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog as fd
 from tkinter import messagebox
 
+
+
 def Select_file(gui):
+
     filetypes = (('csv-files', '*.csv'),('All files', '*.*'))
     filename = fd.askopenfilename(title='Open a file',initialdir='documents/',filetypes=filetypes)
     return filename
 
+
 def Read_file(filename,gui):
+
     #try:
     with open(filename,'r') as file:
-        file = file.read()
+        file_reader = csv.reader(file)
+        Data ={}
+        DATA ={}
+        Header = next(file_reader)
+        for i in Header:
+            Data[i] = []
+
+        #print("data-1", Data)
+        b=0
+        for line in file_reader:
+            A=[]
+
+            for i in line:
+                A.append(i)
+            #print(A)
+            i=0
+
+            for x in Data:
+                Data[x] = A[i]
+                i+=1
+            DATA[b] = Data
+            b+=1
+        print("YOUR DICTIONARY LOOKS LIKE THIS:", DATA)
+
+
+
+
+
+
 
     #except FileNotFoundError:
     #    Label(gui,text="Error! Could not read the file, make sure you selected the right file").pack()
@@ -23,11 +57,16 @@ def Read_file(filename,gui):
     #    Label(gui,text="Error! Could not decrypt the file, make sure you selected the right file").pack()
     #    return False
 
+
 def option_popup(gui):
+
     Choice = tk.messagebox.askyesno(title="Option", message="Your file is invalid. Do you want to try again?")
     return Choice
 
+
+
 def Check_file(file,gui):
+
     #checks if the file selected is ok
     file=str(file)
     if file.endswith('csv'):
